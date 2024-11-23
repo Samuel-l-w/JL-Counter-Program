84eb740472fbe29ca556f19885536c4966ae0b28
Samuel.l.w <samueljrlon@gmail.com>
2024-11-22 19:57:19 -0600

    JL Counter Program
    This counter program helps you keep track of numbers by counting up or down. You can set a starting number and choose how much to add or subtract each time. The program is easy to use and is great for tasks like tracking scores, counting items, or doing basic calculations.

---
+++ index.js
@@ -0,0 +1,23 @@
+// COUNTER PROGRAM
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const counter = document.getElementById("counter");
+
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+    counter.textContent = count;
+}
+
+decreaseBtn.onclick = function() {
+    count--;
+    counter.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    counter.textContent = count;
+}
